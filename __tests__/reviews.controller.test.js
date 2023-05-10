4e1c7e34014323a3f24a64f471ae81e1bab39401
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-10 11:48:36 +0100

    tests for updated reviews

--- __tests__/reviews.controller.test.js
+++ __tests__/reviews.controller.test.js
@@ -12,6 +12,61 @@ afterAll(() => {
   return connection.end();
 });
 
+describe("GET /api/reviews", () => {
+  test("GET /api/reviews should return 200 status code", () => {
+    return request(app).get("/api/reviews").expect(200);
+  });
+  test("GET /api/reviews should return an array", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        expect(Array.isArray(response.body)).toBe(true);
+      });
+  });
+  test("GET /api/reviews should return an an array of objects with the correct props", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        const reviewArray = response.body;
+        const firstReview = reviewArray[0];
+        const fistReviewKeys = Object.keys(firstReview);
+        expect(reviewArray.length === 13).toBe(true);
+        expect(typeof firstReview).toBe("object");
+        expect(fistReviewKeys.length).toBe(9);
+        expect(Object.hasOwn(firstReview, "owner")).toBe(true);
+        expect(Object.hasOwn(firstReview, "title")).toBe(true);
+        expect(Object.hasOwn(firstReview, "review_id")).toBe(true);
+        expect(Object.hasOwn(firstReview, "category")).toBe(true);
+        expect(Object.hasOwn(firstReview, "review_img_url")).toBe(true);
+        expect(Object.hasOwn(firstReview, "created_at")).toBe(true);
+        expect(Object.hasOwn(firstReview, "votes")).toBe(true);
+        expect(Object.hasOwn(firstReview, "designer")).toBe(true);
+        expect(Object.hasOwn(firstReview, "comment_count")).toBe(true);
+      });
+  });
+  test('"GET /api/reviews should return an an array sorted by the date', () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toBeSorted({ key: 'created_at', descending: true });
+      });
+  });
+  test('"GET /api/reviews comment count should be accurate', () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        const exampleFromTestDataWithNoComments = response.body[0];
+        const exampleFromTestDataWithComments = response.body[4];
+        expect(exampleFromTestDataWithNoComments.comment_count).toBe(0);
+        expect(exampleFromTestDataWithComments.comment_count).toBe(3);
+      });
+  });
+});
+
 describe("GET /api/reviews/:review_id", () => {
   test("GET /api/reviews/:review_id should return 200 status code", () => {
     return request(app).get("/api/reviews/1").expect(200);
