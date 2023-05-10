b5008e4c230494e31db36ee9aae5e0fe2c1d737a
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-10 08:57:30 +0100

    Task 4 - review id, updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
 const { getCategories } = require("./controllers/categories.controllers");
+const { getReview } = require("./controllers/reviews.controllers");
 
 app.get("/api", (req, res) => res.status(200).send({ message: "All Ok" }));
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReview);
 
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Endpoint Not Found" })
 );
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  else if (err.code === '22P02') {
+    res.status(400).send({ msg: 'Bad Request' });
+  }
+  else {
+    res.status(500).send({ msg: 'Internal Server Error' });
+  }
+});
 
 module.exports = app;
