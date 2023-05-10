0922f74495def7ec9cd608d2803096477a15802f
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-10 11:48:14 +0100

    controller and model updated for getting reviews array

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -1,4 +1,12 @@
-const { selectReviewById } = require("../models/reviews.models")
+const { selectReviewById, selectAllReviews } = require("../models/reviews.models")
+
+exports.getAllReviews = (req, res, next) => {
+    selectAllReviews()
+    .then((returnedReviews) => {
+        res.status(200).send(returnedReviews);
+    })
+    .catch(next);
+};
 
 exports.getReview = (req, res, next) => {
     const { review_id } = req.params;
