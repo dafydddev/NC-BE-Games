5a9ad19291161969d4edc9f9fbf2e996cff1579c
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-11 08:52:24 +0100

    convert endpoint reading to require

--- controllers/endpoints.controllers.js
+++ controllers/endpoints.controllers.js
@@ -1,9 +1,5 @@
-const fs = require('fs').promises;
+const endpoints = require('../endpoints.json');
 
 exports.readEndpoints = (req, res, next) => {
-    fs.readFile(`${__dirname}/../endpoints.json`)
-    .then((returnedEndpoints) => {
-      const parsedEndpoints = JSON.parse(returnedEndpoints);
-      res.status(200).send(parsedEndpoints);
-    });
-  };
+  res.status(200).send(endpoints);
+};
