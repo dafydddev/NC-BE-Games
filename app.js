42925601c68a5aa3df546f19f1a1ab29932ac6bc
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-11 11:37:34 +0100

    refactored error handling

--- app.js
+++ app.js
@@ -3,32 +3,25 @@ const app = express();
 const { getCategories } = require("./controllers/categories.controllers");
 const { getReview, getAllReviews } = require("./controllers/reviews.controllers");
 const { readEndpoints } = require("./controllers/endpoints.controllers");
-const { getCommentById } = require("./controllers/comments.controllers");
+const { getCommentById, postCommentById } = require("./controllers/comments.controllers");
+const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors");
+
+app.use(express.json());
 
 app.get("/api", readEndpoints);
-
 app.get("/api/categories", getCategories);
 
 app.get("/api/reviews", getAllReviews);
 app.get("/api/reviews/:review_id", getReview);
-
 app.get("/api/reviews/:review_id/comments", getCommentById);
+app.post("/api/reviews/:review_id/comments", postCommentById);
 
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Endpoint Not Found" })
 );
 
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  }
-  else if (err.code === '22P02') {
-    res.status(400).send({ msg: 'Bad Request' });
-  }
-  else {
-    console.log(err);
-    res.status(500).send({ msg: 'Internal Server Error' });
-  }
-});
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
