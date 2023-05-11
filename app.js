906f4c1075df02a7a45c94eab39f55b515479c28
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-11 23:31:06 +0100

    updated app.js with delete

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const { getCategories } = require("./controllers/categories.controllers");
 const { getReview, getAllReviews } = require("./controllers/reviews.controllers");
 const { readEndpoints } = require("./controllers/endpoints.controllers");
-const { getCommentById, postCommentById } = require("./controllers/comments.controllers");
+const { getCommentById, postCommentById, removeComment } = require("./controllers/comments.controllers");
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors");
 
 app.use(express.json());
@@ -15,6 +15,8 @@ app.get("/api/reviews", getAllReviews);
 app.get("/api/reviews/:review_id", getReview);
 app.get("/api/reviews/:review_id/comments", getCommentById);
 app.post("/api/reviews/:review_id/comments", postCommentById);
+
+app.delete("/api/comments/:comment_id", removeComment);
 
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Endpoint Not Found" })
