ca189d30106e7ebd361c31812ff6cf65e7ed2860
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-12 09:16:51 +0100

    updated files got hosting

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,21 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+const config =
+  ENV === "production"
+    ? { connectionString: process.env.DATABASE_URL, max: 2 }
+    : {};
+
+module.exports = new Pool(config);
