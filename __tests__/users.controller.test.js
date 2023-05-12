38dbdb04d0c2b18bf4a30d2ca98c08f8acaea213
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-12 12:30:13 +0100

    updates based on feedback

--- __tests__/users.controller.test.js
+++ __tests__/users.controller.test.js
@@ -21,7 +21,7 @@ describe("GET /api/users", () => {
       .get("/api/users")
       .expect(200)
       .then((response) => {
-        expect(Array.isArray(response.body)).toBe(true);
+        expect(Array.isArray(response.body.users)).toBe(true);
       });
   });
   test("GET /api/users should return an an array of objects with username, name, and avatar_url props", () => {
@@ -29,7 +29,7 @@ describe("GET /api/users", () => {
       .get("/api/users")
       .expect(200)
       .then((response) => {
-        const usersArray = response.body;
+        const usersArray = response.body.users;
         const expectedObjects = usersArray.every((user) => {
           const userKeys = Object.keys(user);
           return (
@@ -39,6 +39,7 @@ describe("GET /api/users", () => {
             Object.hasOwn(user, "avatar_url")
           );
         });
+        expect(usersArray.length).toBe(4)
         expect(expectedObjects).toBe(true);
       });
   });
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,7 +3,7 @@ const { selectUsers } = require("../models/users.models");
 exports.getUsers = (req, res, next) => {
   selectUsers()
     .then((returnedUsers) => {
-      res.status(200).send(returnedUsers);
+      res.status(200).send({users: returnedUsers});
     })
     .catch(next);
 };
