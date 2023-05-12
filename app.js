e8c05151794fc5c6bbee3490a14383748348e276
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-12 11:37:23 +0100

    updated app file for users endpoint

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const { getCategories } = require("./controllers/categories.controllers");
 const { readEndpoints } = require("./controllers/endpoints.controllers");
+const { getUsers } = require("./controllers/users.controllers");
 
 app.get("/api", readEndpoints);
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/users", getUsers);
 
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Endpoint Not Found" })
