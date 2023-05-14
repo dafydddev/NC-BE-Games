a862c280e2507789d6ac4d15b7916c08875bfd84
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-14 11:04:43 +0100

    fixed arrow syntax left over

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,8 +2,6 @@ const { selectUsers } = require("../models/users.models");
 
 exports.getUsers = (req, res, next) => {
   selectUsers()
-    .then((returnedUsers) => {
-      res.status(200).send({users: returnedUsers});
-    })
+    .then((returnedUsers) => res.status(200).send({users: returnedUsers}))
     .catch(next);
 };
