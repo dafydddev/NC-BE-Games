bdc67ab94f672e3bce67154e558aaa6ba2beec47
Dafydd <dafydd@Dafydds-MacBook-Air.local>
2023-05-30 09:48:48 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const app = express();
 
-const reviewsRouter = require('./routers/reviewsRouter');
-const commentsRouter = require('./routers/commentsRouter');
-const usersRouter = require('./routers/usersRouter');
-const categoriesRouter = require('./routers/categoriesRouter');
+const reviewsRouter = require("./routers/reviewsRouter");
+const commentsRouter = require("./routers/commentsRouter");
+const usersRouter = require("./routers/usersRouter");
+const categoriesRouter = require("./routers/categoriesRouter");
 const { readEndpoints } = require("./controllers/endpoints.controllers");
 
 const {
   handleCustomErrors,
   handlePsqlErrors,
   handleServerErrors,
- } = require("./errors");
+} = require("./errors");
+
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/api", readEndpoints);
-app.use('/api/categories', categoriesRouter);
-app.use('/api/reviews', reviewsRouter);
-app.use('/api/comments', commentsRouter);
-app.use('/api/users', usersRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/reviews", reviewsRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/users", usersRouter);
 
 app.get("*", (req, res) =>
   res.status(404).send({ message: "Endpoint Not Found" })
